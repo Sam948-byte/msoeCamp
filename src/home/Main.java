3d4d8f5b2836526a01d2d4f7df994b7cac00ece2
Sam948-byte <techguy763@gmail.com>
2023-07-17 09:57:20 -0500

    summation, but abstracted

---
+++ src/home/Main.java
@@ -0,0 +1,8 @@
+package home;
+import home.Summation;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("The sum is: " + Summation.main(args));
+    }
+}
