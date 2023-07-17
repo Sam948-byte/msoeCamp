bb81d2db5960eea060a61ecf2642ab850da87fde
Sam948-byte <techguy763@gmail.com>
2023-07-17 13:28:38 -0500

    taxes r done

--- src/dayOne/labThree.java
+++ src/dayOne/labThree.java
@@ -7,25 +7,31 @@ public class labThree {
         Scanner in = new Scanner(System.in);
 
         //list of all brackets, first group is single, second married
-        double[][][] filerBrackets = {{{0, 0.10, 1027.5}, {10275, 0.12, 3780}, {41775, 0.22, }, {89075, 0.24}, {170050, 0.32}, {215950, 0.35}, {539900, 0.37}}, {{0, 0.10}, {20550, 0.12}, {83350, 0.22}, {178150, 0.24}, {340100, 0.32}, {431900, 0.35}, {647850, 0.37}}};
+        double[][][] filerBrackets = {{{0, 0.10, 1027.5}, {10275, 0.12, 3780}, {41775, 0.22, 10406}, {89075, 0.24, 19434}, {170050, 0.32, 14688}, {215950, 0.35, 113382.5}, {539900, 0.37}},
+                {{0, 0.10, 2055}, {20550, 0.12, 13816}, {83350, 0.22, 20856}, {178150, 0.24, 38868}, {340100, 0.32, 29376}, {431900, 0.35, 75582.5}, {647850, 0.3766480}}};
 
         //get data from user
         System.out.println("Welcome to the tax calculator!");
-        System.out.println("Please enter your estimated income for 2022: $");
+        System.out.print("Please enter your estimated income for 2022: $");
         double income = in.nextDouble();
         System.out.print("Are you a single filer or a married joint filer (enter 0 for single and 1 for married): ");
         int status = in.nextInt();
 
         //calculate tax
         double tax = 0;
+        int bracket = 0;
         for(int i = 0; i < filerBrackets[status].length; i++) {
-            if(income > filerBrackets[status][i][0] && income < filerBrackets[status][i + 1][0]) {
-                tax += (0.01 * filerBrackets[status][i][1]) *
-                        (income - filerBrackets[status][i][0]);
-            }
+           if(income > filerBrackets[status][i][0]){
+               bracket = i;
+           }
         }
 
+        for(int j = 0; j < bracket; j++) {
+            tax += filerBrackets[status][j][2];
+        }
+        tax += (income - filerBrackets[status][bracket][0]) * filerBrackets[status][bracket][1];
+
         System.out.println("Your estimated tax for 2022 is: $" + tax);
-        System.out.println("Your estimated tax rate for 2022 is: " + (tax / income) * 100 + "%");
+        System.out.println("Your estimated tax rate for 2022 is: " + Math.floor((tax / income) * 100) + "%");
     }
 }
